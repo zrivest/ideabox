c99717f7f6b8d97a6453d49906ad1abd2a22200f
zrivest <zrivest@gmail.com>
2013-08-23 11:55:21 -0500

    first commit for project

---
+++ app.rb
@@ -0,0 +1,44 @@
+require './idea'
+require 'bundler'
+Bundler.require
+
+class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
+  not_found do 
+    erb :error
+  end
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index, locals: {ideas: Idea.all}
+  end
+
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  post '/' do
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save 
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
+    redirect '/'
+  end
+
+  put '/:id' do |id|
+    data = {
+      :title => params['idea_title'],
+      :description => params['idea_description']
+    }
+    Idea.update(id.to_i, data)
+    redirect '/'
+  end
+end
